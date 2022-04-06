b8ebc22e9b63cbe0ed0464bd0e7ae06c4f420cc9
Avishek245 <96076589+Avishek245@users.noreply.github.com>
2022-04-06 20:56:21 +0530

    "1122"

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,18 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
 const App = () => {
   return (
-    <div id="main"></div>
-  )
-}
-
+    <div id="main">
+      <h1 data-ns-test attribute="project-name">
+        Music player
+      </h1>
+      <p data-ns-test attribute="project-description">
+        {" "}
+        description jkskdsd{" "}
+      </p>
+    </div>
+  );
+};
 
 export default App;
